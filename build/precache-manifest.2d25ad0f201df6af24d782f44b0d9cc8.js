b6cc9869cafdfabc85668b0bc6ac86ddd3aa9b03
Mohamed benzarti <m.benzarti.1996@gmail.com>
2022-05-10 19:28:00 +0100

    first commit

---
+++ build/precache-manifest.2d25ad0f201df6af24d782f44b0d9cc8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e56b6b8aada2094898d0928a3f9afa91",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00d4647ba32237ea8b18",
+    "url": "/static/css/2.ef2319b8.chunk.css"
+  },
+  {
+    "revision": "acfc6ce65efea3dabbf5",
+    "url": "/static/css/main.0acb5882.chunk.css"
+  },
+  {
+    "revision": "00d4647ba32237ea8b18",
+    "url": "/static/js/2.c679a9fd.chunk.js"
+  },
+  {
+    "revision": "9cfe38bc4ddb249b37d47a2cde5555b0",
+    "url": "/static/js/2.c679a9fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acfc6ce65efea3dabbf5",
+    "url": "/static/js/main.3634e12d.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "f9ef28fe95d57397cf17c7e8118c87dd",
+    "url": "/static/media/formation2.f9ef28fe.jpg"
+  }
+]);
